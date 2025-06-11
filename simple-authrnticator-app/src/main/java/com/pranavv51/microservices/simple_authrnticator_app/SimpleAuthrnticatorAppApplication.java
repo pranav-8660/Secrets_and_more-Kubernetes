714b5f83c29d6b2f33a68046216bc5a20a425d7a
pranav-8660 <pranavcse2003@gmail.com>
2025-06-11 11:43:38 +0530

    added a base springboot project

---
+++ simple-authrnticator-app/src/main/java/com/pranavv51/microservices/simple_authrnticator_app/SimpleAuthrnticatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.pranavv51.microservices.simple_authrnticator_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAuthrnticatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAuthrnticatorAppApplication.class, args);
+	}
+
+}
---
+++ simple-authrnticator-app/src/test/java/com/pranavv51/microservices/simple_authrnticator_app/SimpleAuthrnticatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranavv51.microservices.simple_authrnticator_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAuthrnticatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
